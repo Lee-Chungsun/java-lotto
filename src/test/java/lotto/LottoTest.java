b9d2ef16f49c7df44afd15aa3a9436895a680097
Lee-Chungsun <cndtjs0218@naver.com>
2021-05-23 21:57:01 +0900

    test : 일급컬렉션 테스트 코드 작성 (기존 테스트 코드는 빌드 오류로 삭제)

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -10,19 +10,13 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoTest {
-    @DisplayName("일치하는 숫자의 갯수를 테스트한다")
+    @DisplayName("로또 생성 후 일급 컬렉션을 통해 당첨 번호와 일치 테스트(로또가 한장이라 가정 후 일치 갯수만 테스트)")
     @Test
-    void countCollectNumber_일치_숫자_갯수_테스트() {
-        Lotto lotto = new Lotto(new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6)));
-        int result = lotto.countCollectNumber(new ArrayList<Integer>(Arrays.asList(1,2,3,4,23,40)));
-        assertThat(result).isEqualTo(4);
-    }
-
-    @DisplayName("랜덤 로또 번호와 당첨 로또 번호의 일치 갯수 테스트")
-    @Test
-    void countRandomCollectNumber_랜덤_숫자_일치_갯수_테스트() {
-        Lotto lotto = new Lotto(() -> Arrays.asList(1,2,3,4,23,45));
-        int result = lotto.countCollectNumber(new ArrayList<Integer>(Arrays.asList(1,2,3,4,23,40)));
-        assertThat(result).isEqualTo(5);
+    void countFirstCollectionLottoColle_로또_일급_컬렉션_사이즈_테스트() {
+        List<Lotto> listLotto = new ArrayList<Lotto>();
+        listLotto.add(new Lotto(() -> Arrays.asList(16,17,23,25,33,45)));
+        Lottos lottos = new Lottos(listLotto);
+        int result = lottos.countCollectNumber(new ArrayList<Integer>(Arrays.asList(12,18,23,33,40,41)));
+        assertThat(result).isEqualTo(2);
     }
 }
