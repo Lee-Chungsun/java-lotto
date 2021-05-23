1df2c62f65be44a57649257f2bb201bcb5e8f86c
Lee-Chungsun <cndtjs0218@naver.com>
2021-05-23 18:05:39 +0900

    docs : 3단계 요구사항 README.md 작성

---
+++ src/main/java/lotto/LottoRandomUtils.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class LottoRandomUtils {
+    private static final List<Integer> lottoRange = new ArrayList<Integer>();
+
+    public LottoRandomUtils(){
+        for (int number = 1; number < 46; number++) {
+            lottoRange.add(number);
+        }
+    }
+
+    public List<Integer> createRandomLotto() {
+        Collection.
+    }
+}
