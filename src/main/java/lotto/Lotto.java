5a1d1011246a079db5644e7b000ad7d0373b20b0
Lee-Chungsun <cndtjs0218@naver.com>
2021-05-23 18:17:17 +0900

    test : 1~45 사이의 숫자와 정답 숫자의 일치 갯수 테스트

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,2 +1,24 @@
-package lotto;public class Lotto {
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numberList;
+
+    public Lotto(List<Integer> numberList) {
+        this.numberList = numberList;
+    }
+
+    public int countCollectNumber(List<Integer> winLotto) {
+        int result = 0;
+        for (Integer lotto : winLotto) {
+            result += searchCollectNumber(lotto);
+        }
+        return result;
+    }
+
+    private int searchCollectNumber(Integer lotto) {
+        return numberList.contains(lotto) ? 1 : 0;
+    }
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,2 +1,19 @@
-package lotto;public class LottoTest {
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoTest {
+    @DisplayName("일치하는 숫자의 갯수를 테스트한다")
+    @Test
+    void countCollectNumber_일치_숫자_갯수_테스트() {
+        Lotto lotto = new Lotto(new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6)));
+        int result = lotto.countCollectNumber(new ArrayList<Integer>(Arrays.asList(1,2,3,4,23,40)));
+        assertThat(result).isEqualTo(4);
+    }
 }
